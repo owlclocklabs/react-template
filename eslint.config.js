0db3c6338b2d2a6173cb70dee4d6434260b69ce0
Mike Skalandunas <mskalandunas@gmail.com>
2024-10-23 21:26:49 -0400

    update linting rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,32 @@
-import js from '@eslint/js'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       parserOptions: {
-        ecmaVersion: 'latest',
+        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
-    settings: { react: { version: '18.3' } },
+    settings: {
+      react: {
+        version: "18.3",
+      },
+    },
     plugins: {
       react,
-      'react-hooks': reactHooks
+      "react-hooks": reactHooks,
     },
     rules: {
-      ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
+      ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off'
+      "react/jsx-no-target-blank": "off",
     },
   },
-]
+];
